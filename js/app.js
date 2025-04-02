a767786619301e477b48ee6c0b1f90e797c7b2c4
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-04-02 10:41:36 +0200

    Refactor Lotto class to create ball elements dynamically and update HTML structure

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 class Lotto{
+    numbers
     constructor(){
         this.numbers = [];
     }
@@ -11,13 +12,30 @@ class Lotto{
             }
         }
         this.numbers.sort((a, b) => a - b);
+        this.createBalls()
     }
     getNumbers(){
         return this.numbers;
     }
     
+
+    createBalls(){
+        const wrapper=document.querySelector("#wrapper")
+        const row = document.createElement("div");
+        row.classList.add("ball-row");
+        wrapper.appendChild(row);
+        for(let ball of this.numbers){
+            const ballElement = document.createElement("div");
+            ballElement.classList.add("ball");
+            const text = document.createElement("span");
+            text.textContent = ball;
+            ballElement.appendChild(text);
+            row.appendChild(ballElement);
+        }
+
+        
+    }
 }
 
 const lotto = new Lotto();
 lotto.generateNumbers();
-console.log(lotto.getNumbers());
