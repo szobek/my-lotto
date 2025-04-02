eb50d92a3e542d1bb2db8eb8ca631714a4cfc977
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-04-02 14:47:21 +0200

    Add dashboard to display drawn numbers and their frequency

--- js/app.js
+++ js/app.js
@@ -1,32 +1,35 @@
-class Lotto{
+class Lotto {
+    lastNumbers;
     numbers
-    constructor(){
+    constructor() {
+        this.lastNumbers = {};
         this.numbers = [];
         this.createEventListeners()
     }
-    generateNumbers(){
+    generateNumbers() {
         this.numbers = [];
-        while(this.numbers.length < 5){
+        while (this.numbers.length < 5) {
             let randomNumber = Math.floor(Math.random() * 90) + 1;
-            if(!this.numbers.includes(randomNumber)){
+            if (!this.numbers.includes(randomNumber)) {
                 this.numbers.push(randomNumber);
             }
         }
         this.numbers.sort((a, b) => a - b);
         this.createBalls()
+        this.createDashboardFromNums()
     }
-    getNumbers(){
+    getNumbers() {
         return this.numbers;
     }
-    
 
-    createBalls(){
-        const wrapper=document.querySelector("#wrapper")
+
+    createBalls() {
+        const wrapper = document.querySelector("#wrapper")
         wrapper.innerHTML = "";
         const row = document.createElement("div");
         row.classList.add("ball-row");
         wrapper.appendChild(row);
-        for(let ball of this.numbers){
+        for (let ball of this.numbers) {
             const ballElement = document.createElement("div");
             ballElement.classList.add("ball");
             const text = document.createElement("span");
@@ -35,18 +38,65 @@ class Lotto{
             row.appendChild(ballElement);
         }
     }
-    createEventListeners(){
+    createEventListeners() {
         document.getElementById("sorsolo-btn")
-        .addEventListener("click",()=>{
-            sorsol()
-        });
+            .addEventListener("click", () => {
+                sorsol()
+            });
     }
-    
+
+    createDashboardFromNums() {
+        const wrapper = document.getElementById("dashboard")
+        wrapper.innerHTML = "";
+        this.numbers.forEach((num) => {
+            if (this.lastNumbers[num]) {
+                this.lastNumbers[num]++;
+            } else {
+                this.lastNumbers[num] = 1;
+            }
+        })
+        // console.log(this.lastNumbers);
+        const table = document.createElement("table");
+        const thead = document.createElement("thead");
+        const tbody = document.createElement("tbody");
+
+        table.appendChild(thead);
+        table.appendChild(tbody);
+
+        const headerRow = document.createElement("tr");
+        const headerCell = document.createElement("th");
+        const headerCell2 = document.createElement("th");
+        headerCell.textContent = "Számok";
+        headerCell2.textContent = "húzva";
+        headerRow.appendChild(headerCell);
+        headerRow.appendChild(headerCell2);
+        thead.appendChild(headerRow);
+
+        wrapper.appendChild(table);
+
+        const rows = [];
+        for (const num in this.lastNumbers) {
+            const row = document.createElement("tr");
+            const cell = document.createElement("td");
+            const cell2 = document.createElement("td");
+            cell.textContent = num;
+            cell2.textContent = this.lastNumbers[num];
+            row.appendChild(cell);
+            row.appendChild(cell2);
+            rows.push(row);
+        }
+
+        tbody.append(...rows);
+
+
+
+    }
+
 }
 
 const lotto = new Lotto();
-const sorsol=()=>{
-    lotto.generateNumbers();            
+const sorsol = () => {
+    lotto.generateNumbers();
 }
 
 
